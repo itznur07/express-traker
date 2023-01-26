250793c4bd4f1e3da4ca2a84cc37dd831ab743ba
Md Nuruddin <nuruddinalways99@gmail.com>
2023-01-26 10:24:49 +0600

    Code Refactor

--- src/Components/TransactionLists.js
+++ src/Components/TransactionLists.js
@@ -4,10 +4,10 @@ const TransactionLists = () => {
   return (
     <>
       <h3>History</h3>
-      <ul id='list' class='list'>
-        <li class='minus'>
+      <ul  class='list'>
+        <li className='minus'>
           Cash <span>-$400</span>
-          <button class='delete-btn'>x</button>
+          <button className='delete-btn'>x</button>
         </li>
       </ul>
     </>
