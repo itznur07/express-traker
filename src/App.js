8ff445d2622d60326e0155f82447da1d3fddc4b1
Md Nuruddin <nuruddinalways99@gmail.com>
2023-01-26 10:10:41 +0600

    Income Express component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Balance from "./Components/Balance";
 import Header from "./Components/Header";
+import IncExp from "./Components/IncExp";
 
 function App() {
   return (
     <div>
       <Header />
-      <div className='container'>
-        <Balance />
+      <div className="container">
+      <Balance />
+      <IncExp />
       </div>
     </div>
   );
