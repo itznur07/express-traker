9dcfa73cae6f1d1013f2adfb582bca2d80cc4225
Md Nuruddin <nuruddinalways99@gmail.com>
2023-01-26 10:13:52 +0600

    Transaction History componet

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import Balance from "./Components/Balance";
 import Header from "./Components/Header";
 import IncExp from "./Components/IncExp";
+import TransactionLists from "./Components/TransactionLists";
 
 function App() {
   return (
     <div>
       <Header />
-      <div className="container">
-      <Balance />
-      <IncExp />
+      <div className='container'>
+        <Balance />
+        <IncExp />
+        <TransactionLists />
       </div>
     </div>
   );
